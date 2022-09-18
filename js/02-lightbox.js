1570fa8001c85c83f1f76a62f5ac787d0d7d8917
Oleksii-Kukharuk <104523043+Oleksii-Kukharuk@users.noreply.github.com>
2022-09-18 19:40:49 +0300

    almost don scond task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallryRef = document.querySelector(".gallery");
+
+function galleryMarkup(items) {
+  return items
+    .map(
+      (item) => `<a class="gallery__item" src="${item.original}">
+    <img class="gallery__image" src="${item.preview}"  alt="${item.description}">
+    </a></div>`
+    )
+    .join("");
+}
+
+const addMarkup = galleryMarkup(galleryItems);
+gallryRef.innerHTML = addMarkup;
+
+gallryRef.addEventListener("click", imgSelect);
+
+function imgSelect(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  let lightbox = new SimpleLightbox(".gallery a");
+}
